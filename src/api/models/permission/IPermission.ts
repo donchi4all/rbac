61b92e5221dbb22a0f1c6728cd89e4ad11f84b58
Victor Kiraydt <v.kiraydt@tapston.com>
2021-04-01 18:12:01 +0300

    Add Permission model

---
+++ src/api/models/permission/IPermission.ts
@@ -0,0 +1,13 @@
+import { Optional } from 'sequelize';
+
+export interface PermissionInterface {
+  id?: number;
+  title: string;
+  description?: string;
+  slug: string;
+  active: boolean;
+  createdAt?: Date,
+  updatedAt?: Date;
+}
+
+export type PermissionCreationType = Optional<PermissionInterface, 'id' | 'description' | 'createdAt' | 'updatedAt'>;
