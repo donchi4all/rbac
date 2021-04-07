c6ee0d4399eea92d056286ce15d35af12375ea0c
Victor Kiraydt <v.kiraydt@tapston.com>
2021-04-07 12:04:42 +0300

    Udpdate Role table structure

--- src/api/models/role/IRole.ts
+++ src/api/models/role/IRole.ts
@@ -4,7 +4,6 @@ export interface RoleInterface {
   id?: number;
   title: string;
   description?: string;
-  slug: string;
   active: boolean;
   createdAt?: Date,
   updatedAt?: Date;
--- src/api/models/role/index.ts
+++ src/api/models/role/index.ts
@@ -1,11 +1,12 @@
 import { Table, AutoIncrement, PrimaryKey, Default, Column, Model, AllowNull, CreatedAt, UpdatedAt, DataType } from 'sequelize-typescript';
 import { RoleInterface, RoleCreationType } from './IRole';
 
-@Table
+@Table({
+  tableName: 'role',
+})
 export class Role extends Model<RoleInterface, RoleCreationType> {
   @PrimaryKey
   @AutoIncrement
-  @Default(0)
   @Column(DataType.INTEGER)
   id: number;
 
@@ -16,10 +17,6 @@ export class Role extends Model<RoleInterface, RoleCreationType> {
   @Default('')
   @Column(DataType.STRING(255))
   description: string;
-
-  @AllowNull(false)
-  @Column(DataType.STRING(48))
-  slug: string;
 
   @Default(true)
   @AllowNull(false)
