fb0eff1d07d8efd4001e9693d2b7c4dd8436f2c6
Victor Kiraydt <v.kiraydt@tapston.com>
2021-04-07 12:05:42 +0300

    Add Access Control module

--- src/modules/access-control/index.ts
+++ src/modules/access-control/index.ts
@@ -1,15 +1,42 @@
-// TEMPORARY file
-
 import ac from 'accesscontrol';
 
-import Database from '../database';
+import * as Models from '../../api/models';
+import { LoggerDecorator, LoggerInterface } from '../logger';
 
-class AccessControl {
+export default class AccessControl {
+  @LoggerDecorator('AccessControl')
+  private log: LoggerInterface;
   private accessControl: ac.AccessControl;
 
   constructor () {
-    this.accessControl = new ac.AccessControl();
+    try {
+      const grantList = [
+        { role: 'admin', resource: 'transaction', action: 'create:any', attributes: '*' },
+        { role: 'admin', resource: 'transaction', action: 'read:any', attributes: '*' },
+        { role: 'admin', resource: 'transaction', action: 'update:any', attributes: '*' },
+        { role: 'admin', resource: 'transaction', action: 'delete:any', attributes: '*' },
+        // { role: 'admin', resource: 'transaction', action: 'execute:any', attributes: '*' },
+        // { role: 'admin', resource: 'transaction', action: 'approve:any', attributes: '*' },
+      ];
+      this.accessControl = new ac.AccessControl(grantList);
+      this.log.info('Access Control has been initialized with grant list successfully');
+    } catch (err) {
+      this.log.error(`There is an error with Access Control initialization: ${err}`);
+      throw err;
+    }
+  }
+
+  public async permission (roleId: number): Promise<unknown> {
+    try {
+    const rolePrivilege = await Models.RolePrivilege.findOne({
+      where: { idRole: roleId },
+      include: [ Models.Grant, Models.GrantType, Models.Permission, Models.Role ],
+    });
+    // return this.accessControl.can(rolePrivilege);
+    return rolePrivilege;
+    } catch (err) {
+      this.log.error(`There is an error with getting permission for role ID ${roleId}: ${err}`);
+      throw err;
+    }
   }
 }
-
-export default new AccessControl;
