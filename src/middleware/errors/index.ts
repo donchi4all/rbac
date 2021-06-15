bf0593bb7d56c083f9e5f7e14f3e098846faf337
Victor Kiraydt <v.kiraydt@tapston.com>
2021-06-15 11:26:45 +0300

    Update error middleware

--- src/middleware/errors/index.ts
+++ src/middleware/errors/index.ts
@@ -1,4 +1,3 @@
-
 import express from 'express';
 
 import { ErrorInterface } from '../../modules/exceptions/IError';
@@ -11,7 +10,11 @@ class Errors {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     next: express.NextFunction): void {
       const { status } = err;
-      res.status(status || 500).json(err);
+      res.status(status || 500).json({
+        ...err,
+        message: err.message,
+        status: undefined,
+      });
   }
 }
 
